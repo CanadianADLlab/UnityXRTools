3423f56bb9a76e8965c1cb8773f0638a4bba5e30
Shaun McGowan <shaun_mcgowan@hotmail.com>
2020-12-16 10:50:53 -0500

    Added some cool hide/show public props stuff

---
+++ Assets/Project/Scripts/Interaction/XRInteractableHands.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using XRCrossPlatformInput;
+
+namespace XRCrossPlatformInput
+{
+    /// <summary>
+    /// Attactch with interactable objects to hide handss
+    /// </summary>
+    
+    [RequireComponent(typeof(InteractableObject))]
+    public class XRInteractableHands : MonoBehaviour
+    {
+        public GameObject LeftHandMesh;
+        public GameObject RightHandMesh;
+
+        public bool isTwoHands = false;
+
+        [DrawIf("isTwoHands", true)]  // the user doesn't need to see these if no hands
+        public GameObject SecondaryLeftHandMesh;
+        [DrawIf("isTwoHands", true)] 
+        public GameObject SecondaryRightHandMesh;
+
+
+
+
+        private InteractableObject interactable;
+        private InteractableSecondaryGrab secondaryInteractable; // not needed but some things might want to be two handed
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            interactable = GetComponent<InteractableObject>();
+            secondaryInteractable = GetComponent<InteractableSecondaryGrab>();
+          
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void ShowHands() // Checks to see whats grabbing and from there enables
+        {
+
+        }
+    }
+}
+
+
+
