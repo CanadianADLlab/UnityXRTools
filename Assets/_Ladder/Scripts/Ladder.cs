78329c009260def907f75731370f53403d545538
Shaun McGowan <shaun_mcgowan@hotmail.com>
2021-01-13 10:26:31 -0500

    added the ladder package thanks chris

---
+++ Assets/_Ladder/Scripts/Ladder.cs
@@ -0,0 +1,154 @@
+﻿using EpicXRCrossPlatformInput;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    Rigidbody playerRB = null;
+    bool climbing = false;
+
+    List<Ladder_Rung> rungs = new List<Ladder_Rung>();
+
+    Transform trackerTransLeft = null;
+    Transform trackerTransRight = null;
+   
+    Vector3 lastPosLeft = Vector3.zero;
+    Vector3 lastPosRight = Vector3.zero;
+
+    bool leftHandClimb = true;
+
+    bool isCurrentLadder = false;
+
+    List<Ladder> ladders = new List<Ladder>();
+
+    private void Start()
+    {
+        playerRB = XRPositionManager.Instance.PlaySpace.GetComponentInChildren<Rigidbody>();
+        rungs.AddRange(GetComponentsInChildren<Ladder_Rung>());
+
+        ladders.AddRange(GameObject.FindObjectsOfType<Ladder>());
+
+        for (int i = 0; i < rungs.Count; i++)
+        {
+            rungs[i].SetLadder(this,i);
+        }
+    }
+
+    private void Update()
+    {
+        if (playerRB == null)
+            return;
+        Climbing();
+        Gravity();
+    }
+
+    void Gravity()
+    {
+        if(climbing)
+        {
+            playerRB.velocity = Vector3.zero;
+        }
+
+        //Gravity controll**********************
+        if (climbing && playerRB.useGravity && isCurrentLadder)
+        {
+            playerRB.useGravity = false;
+        }
+        if (!climbing && !playerRB.useGravity && isCurrentLadder)
+        {
+            playerRB.useGravity = true;
+        }
+        //**************************************
+        climbing = false;
+    }
+    void Climbing()
+    {
+        //trigger down********************************
+        if (trackerTransLeft != null)
+        {
+            if (XRCrossPlatformInputManager.Instance.GetInputByButton(ButtonTypes.Trigger, ControllerHand.Left, false))
+            {
+                lastPosLeft = trackerTransLeft.position;
+                leftHandClimb = true;
+                BecomeCurrentLadder();
+                climbing = true;
+            }
+        }
+
+
+        if (trackerTransRight != null)
+        {
+            if (XRCrossPlatformInputManager.Instance.GetInputByButton(ButtonTypes.Trigger, ControllerHand.Right, false))
+            {
+                lastPosRight = trackerTransRight.position;
+                leftHandClimb = false;
+                BecomeCurrentLadder();
+                climbing = true;
+            }
+        }
+        //********************************************
+
+        //trigger hold****************************************
+        if (trackerTransLeft != null && leftHandClimb)
+        {
+            //trigger hold
+            if (XRCrossPlatformInputManager.Instance.GetInputByButton(ButtonTypes.Trigger, ControllerHand.Left, true))
+            {
+                Vector3 _climbVector = trackerTransLeft.position - lastPosLeft;
+                XRPositionManager.Instance.PlaySpace.transform.Translate(-_climbVector);
+                climbing = true;
+            }
+
+            lastPosLeft = trackerTransLeft.position;
+        }
+
+        if (trackerTransRight != null && !leftHandClimb)
+        {
+            //trigger hold
+            if (XRCrossPlatformInputManager.Instance.GetInputByButton(ButtonTypes.Trigger, ControllerHand.Right, true))
+            {
+                Vector3 _climbVector = trackerTransRight.position - lastPosRight;
+                XRPositionManager.Instance.PlaySpace.transform.Translate(-_climbVector);
+                climbing = true;
+            }
+
+            lastPosRight = trackerTransRight.position;
+        }
+        //********************************************
+    }
+
+    public void SetLeftHand(Transform _leftHandTrans)
+    {
+        trackerTransLeft = _leftHandTrans;
+        lastPosLeft = trackerTransLeft.position;
+    }
+    public void SetRightHand(Transform _rightHandTrans)
+    {
+        trackerTransRight = _rightHandTrans;
+        lastPosRight = trackerTransRight.position;
+    }
+
+    public void SetLeftHand()
+    {
+        trackerTransLeft = null;
+    }
+    public void SetRightHand()
+    {
+        trackerTransRight = null;
+    }
+
+    public void SetCurrentLadder(bool _isCurLadder)
+    {
+        isCurrentLadder = _isCurLadder;
+    }
+
+    void BecomeCurrentLadder()
+    {
+        for (int i = 0; i < ladders.Count; i++)
+        {
+            ladders[i].SetCurrentLadder(false);
+        }
+        isCurrentLadder = true;
+    }
+
+}
---
+++ Assets/_Ladder/Scripts/Ladder_Rung.cs
@@ -0,0 +1,50 @@
+﻿using EpicXRCrossPlatformInput;
+using UnityEngine;
+
+public class Ladder_Rung : MonoBehaviour
+{
+
+
+    Ladder ladder;
+    int rungIndex;
+
+    public void SetLadder(Ladder _ladder, int _rungIndex)
+    {
+        ladder = _ladder;
+        rungIndex = _rungIndex;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 12)//controller layer
+        {
+            bool _isLeftHand = other.gameObject.GetComponent<Controller>().Hand == ControllerHand.Left;
+            if (_isLeftHand)
+            {
+                ladder.SetLeftHand(other.gameObject.transform);
+            }
+            else
+            {
+                ladder.SetRightHand(other.gameObject.transform);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 12)//controller layer
+        {
+            bool _isLeftHand = other.gameObject.GetComponent<Controller>().Hand == ControllerHand.Left;
+            if (_isLeftHand)
+            {
+                ladder.SetLeftHand();
+            }
+            else
+            {
+                ladder.SetRightHand();
+            }
+        }
+    }
+
+   
+}
