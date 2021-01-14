aae2ca650f089580ced96ce96d9ec82e766d40b4
Shaun McGowan <shaun_mcgowan@hotmail.com>
2021-01-14 14:39:17 -0500

    added on off switch to tractor beam with magnetic grabbing

--- Assets/Project/Scripts/Interaction/XRMagnetGrab.cs
+++ Assets/Project/Scripts/Interaction/XRMagnetGrab.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace EpicXRCrossPlatformInput
 {
@@ -7,18 +8,25 @@ namespace EpicXRCrossPlatformInput
     /// </summary>
     public class XRMagnetGrab : MonoBehaviour
     {
+        [Header("Controls")]
+        [Tooltip("Holding this button will enable the ability to aim and grab")]
+        public ButtonTypes MagneticGrabInput = ButtonTypes.Primary;
         [Header("Image Prefab")]
         public GameObject GrabIconPrefab; // Little icon that displays on item when raycasted at
 
-        [Header("Sphere Cast Settings")]
-
+        [Header("RayCast Settings")]
         public float GrabDistance = 10; // How far the controller can grab from in mtrs
         public LayerMask GrabLayer;
-        public Vector3 SphereCastDirection = new Vector3(0,0,1);
-        public float SphereCastRadius = 0.15f;
+        public Vector3 RaycastDirection = new Vector3(0, 0, 1);
+
+        [Header("Tractor Beam")]
+        public float MinTractorBeamZ = 5.0f; // Min length when the beam is enabled
+        public GameObject TractorBeamPrefab;
 
 
+        private GameObject tractorBeam;
         private Controller xrGamePad;
+        private bool isMagneticGrabbing = false;
 
         private GameObject handSprite;
         private InteractableObject objectToBeGrabbed;
@@ -31,23 +39,44 @@ namespace EpicXRCrossPlatformInput
                 handSprite = GameObject.Instantiate(GrabIconPrefab);
                 ToggleHandIcon(false);
             }
-          
+            ResetBeam();
+        }
+
+        private void ResetBeam()
+        {
+            if (tractorBeam == null)
+            {
+                tractorBeam = GameObject.Instantiate(TractorBeamPrefab, Vector3.zero, Quaternion.identity, this.transform);
+                tractorBeam.transform.localScale = new Vector3(tractorBeam.transform.localScale.x, tractorBeam.transform.localScale.y, MinTractorBeamZ);
+                tractorBeam.transform.localRotation = Quaternion.identity;
+                tractorBeam.transform.forward = RaycastDirection;
+            }
+            tractorBeam.transform.localPosition = new Vector3(0, 0, MinTractorBeamZ / 2);
+            tractorBeam.SetActive(false);
         }
 
         private void DrawDebugRay()
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(transform.TransformDirection(SphereCastDirection)));
+            Debug.DrawRay(transform.position, transform.TransformDirection(transform.TransformDirection(RaycastDirection)));
         }
         private void Update()
         {
+            GetInput();
             CastRayFromController();
             DrawDebugRay();
             CheckIfGrabbed();
         }
 
+
+
+        private void GetInput()
+        {
+            isMagneticGrabbing = XRCrossPlatformInputManager.Instance.GetInputByButton(MagneticGrabInput, GetComponent<Controller>().Hand, true);
+        }
+
         private void CheckIfGrabbed()
         {
-            if(objectToBeGrabbed != null && objectToBeGrabbed.IsGrabbed) // Basically if we are grabbing I don't even want to let users do things
+            if (objectToBeGrabbed != null && objectToBeGrabbed.IsGrabbed) // Basically if we are grabbing I don't even want to let users do things
             {
                 ToggleHandIcon(false);
             }
@@ -55,31 +84,35 @@ namespace EpicXRCrossPlatformInput
 
         private void CastRayFromController()
         {
-            if(!xrGamePad.IsGrabbing)
+            if (isMagneticGrabbing && !xrGamePad.IsGrabbing)
             {
                 RaycastHit hit;
-                if(Physics.SphereCast(transform.position,SphereCastRadius,transform.TransformDirection(SphereCastDirection),out hit,GrabDistance,GrabLayer))
+                print("True setting active");
+                tractorBeam.SetActive(true);
+                if (Physics.Raycast(transform.position,transform.TransformDirection(RaycastDirection), out hit, GrabDistance, GrabLayer))
                 {
                     // could be the object, could be a parent, could be a child who knows lets be certain though
+                    tractorBeam.transform.localScale = new Vector3(tractorBeam.transform.localScale.x, tractorBeam.transform.localScale.y, hit.distance);
+                    tractorBeam.transform.localPosition = new Vector3(0, 0, tractorBeam.transform.localScale.z / 2);
                     if (hit.collider.transform.GetComponent<InteractableObject>() != null)
                     {
                         objectToBeGrabbed = hit.collider.transform.GetComponent<InteractableObject>();
                     }
-                    else if(hit.collider.transform.GetComponentInChildren<InteractableObject>() != null)
+                    else if (hit.collider.transform.GetComponentInChildren<InteractableObject>() != null)
                     {
                         objectToBeGrabbed = hit.collider.transform.GetComponentInChildren<InteractableObject>();
                     }
-                    else if(hit.collider.transform.GetComponentInParent<InteractableObject>() != null)
+                    else if (hit.collider.transform.GetComponentInParent<InteractableObject>() != null)
                     {
                         objectToBeGrabbed = hit.collider.transform.GetComponentInParent<InteractableObject>();
                     }
-                 
 
-                    if (objectToBeGrabbed != null && objectToBeGrabbed.SnapToController ) // we hit a grabable boy and we need to make sure it has snap mode 
+
+                    if (objectToBeGrabbed != null && objectToBeGrabbed.SnapToController) // we hit a grabable boy and we need to make sure it has snap mode 
                     {
                         ToggleHandIcon(true);
                         objectToBeGrabbed.CheckGrab(GetComponent<Collider>()); // This function handles grabbing, uses the controller collider to attatch
-                      
+
                         if (!objectToBeGrabbed.HoldToGrab && objectToBeGrabbed.IsGrabbed) // if in the above function the user grabs the thing we need to turn off the icon
                         {
                             ToggleHandIcon(false);
@@ -98,9 +131,16 @@ namespace EpicXRCrossPlatformInput
                     ToggleHandIcon(false);
                 }
             }
+            else
+            {
+                print("Else");
+                objectToBeGrabbed = null;
+                ToggleHandIcon(false);
+                ResetBeam();
+            }
         }
 
-       
+
         private void ToggleHandIcon(bool onOff)
         {
             if (handSprite != null)
